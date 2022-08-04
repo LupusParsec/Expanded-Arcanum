f86d9ee25201d5d5e82e4e24edb364184e78c944
Uris Herialos <100441957+LupusParsec@users.noreply.github.com>
2022-08-04 00:14:39 -0300

    Update ClientEventsCC.java

--- src/main/java/com/mr208/ea/modules/chococraft/ClientEventsCC.java
+++ src/main/java/com/mr208/ea/modules/chococraft/ClientEventsCC.java
@@ -1,1 +1,19 @@
+package com.mr208.ea.modules.chococraft;
 
+import net.chococraft.utils;
+import com.mr208.ea.ExpandedArcanum;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ClientEventsCC
+{
+	public ClientEventsCC()
+	{
+	
+	}
+	
+	public static ClientEventsCC INSTANCE = new ClientEventsCC();
+	
+}
+Footer
+© 2022 GitHub, Inc.
